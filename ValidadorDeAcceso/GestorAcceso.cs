ad37f586e35d382001f2898c5dce86f6792a953d
brayan <tovarromobrayangiovanni@gmail.com>
2026-02-08 01:06:31 -0600

    Añade clases Usuario y GestorAcceso al proyecto
    Se incorporan los archivos Usuario.cs y GestorAcceso.cs para gestionar usuarios y validar el acceso con control de intentos fallidos. Se actualizan los archivos de solución y proyecto para incluir las nuevas clases y el identificador del proyecto.

---
+++ ValidadorDeAcceso/GestorAcceso.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidadorDeAcceso
+{
+    internal class GestorAcceso
+    {
+        private List<Usuario> usuarios;
+        private int intentosFallidos = 0;
+        private const int MAX_INTENTOS = 3;
+
+        // Constructor para inicializar la lista de usuarios
+        public GestorAcceso()
+        {
+            usuarios = new List<Usuario>();
+            // Agregar algunos usuarios de ejemplo
+
+        }
+        public GestorAcceso(List<Usuario> usuarios)
+        {
+            this.usuarios = usuarios;
+        }
+
+        public void AgregarUsuario(Usuario usuario)
+        {
+            usuarios.Add(usuario);
+
+        }
+        public bool ValidarAcceso(string nombre, string contraseña)
+        {
+            if (intentosFallidos > MAX_INTENTOS)
+            {
+                StringBuilder csb = new StringBuilder();
+                csb.AppendLine("Demasiados intentos fallidos. Acceso bloqueado.");
+                return false;
+            }
+            foreach (var usuario in usuarios)
+            {
+                if (usuario.Nombre == nombre && usuario.Contraseña == contraseña)
+                {
+                    StringBuilder sbb = new StringBuilder();
+                    sbb.AppendLine("Acceso concedido. Bienvenido, " + nombre + "!");
+                    intentosFallidos = 0; // Reiniciar el contador de intentos fallidos
+                    return true;
+                }
+            }
+            intentosFallidos++;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Acceso denegado. Intento fallido #" + intentosFallidos);
+            return false;
+
+        }
+    }
+}
---
+++ ValidadorDeAcceso/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidadorDeAcceso
+{
+    internal class Usuario
+    {
+        private string nombre="";  
+        private string contraseña="";
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }   
+        public string Contraseña
+        {
+            get { return contraseña; }
+            set { contraseña = value; }
+        }   
+
+
+        // Constructor
+        public Usuario(string nombre, string contraseña)
+        {
+            this.nombre = nombre;
+            this.contraseña = contraseña;
+        }
+    }
+}
